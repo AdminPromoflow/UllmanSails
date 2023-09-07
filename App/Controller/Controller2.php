e05cb82d3b77520e2304400d691808458808cb05
laura <laurazoro@gmail.com>
2023-09-07 05:24:08 -0500

    ...

--- App/Controller/Controller2.php
+++ App/Controller/Controller2.php
@@ -19,7 +19,38 @@ require_once('../Models/Searchs.php');
       echo $pages;
     }
     elseif ($_POST['module'] == "sendEmail") {
-    echo "holaholaholahola a ver";
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Check if a file was uploaded
+        if (isset($_FILES["file"])) {
+            $file = $_FILES["file"];
+
+            // Check if there were no errors during the upload
+            if ($file["error"] === UPLOAD_ERR_OK) {
+                // Check if the uploaded file is a PDF
+                $allowed_extensions = ["pdf"];
+                $file_extension = pathinfo($file["name"], PATHINFO_EXTENSION);
+
+                if (in_array($file_extension, $allowed_extensions)) {
+                    // Read the contents of the PDF file into a variable
+                    $pdf_contents = file_get_contents($file["tmp_name"]);
+
+                    // Now $pdf_contents contains the PDF file data
+                    // You can further process $pdf_contents as needed
+
+                    echo "The PDF file has been uploaded and stored in a variable.";
+                } else {
+                    echo "Invalid file type. Only PDF files are allowed.";
+                }
+            } else {
+                echo "Error uploading the file: " . $file["error"];
+            }
+        } else {
+            echo "No file was sent.";
+        }
+    } else {
+        echo "Invalid request.";
+    }
+
     }
 
 
