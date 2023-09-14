206b4b52aead1b461c6ba49880aa7a209b1b44b4
laura <laurazoro@gmail.com>
2023-09-14 04:36:26 -0500

    ...

--- App/Controller/SendEmail.php
+++ App/Controller/SendEmail.php
@@ -1,7 +1,7 @@
 <?php
-  use PHPMailer/PHPMailer/PHPMailer;
-  use PHPMailer/PHPMailer/Exception;
-  use PHPMailer/PHPMailer/SMTP;
+  use PHPMailer\PHPMailer\PHPMailer;
+  use PHPMailer\PHPMailer\Exception;
+  use PHPMailer\PHPMailer\SMTP;
 
   require '../PHPMailer-master/src/PHPMailer.php';
   require '../PHPMailer-master/src/Exception.php';
